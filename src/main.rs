711141bf44dd6732cbca12c72363e1cf74c902c5
Troy Neubauer <troyneubauer@gmail.com>
2022-02-03 21:10:22 -0500

    Finished bench on arm micro controller

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 #![feature(bench_black_box)]
-#![no_std]
-use benchmark_simple::*;
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), no_main)]
+#[cfg(not(feature = "std"))]
+use panic_halt as _;
+#[cfg(not(feature = "std"))]
+use cortex_m_rt::entry;
+#[cfg(not(feature = "std"))]
+use cortex_m_semihosting::hprintln;
+#[cfg(not(feature = "std"))]
+use stm32f4xx_hal as hal;
+#[cfg(not(feature = "std"))]
+use hal::{delay::Delay, pac, prelude::*};
+
+use core::time::Duration;
+#[cfg(feature = "std")]
+use std::time::Instant;
+
+//use benchmark_simple::*;
 use generic_array::GenericArray;
 
 /// Benchmark function signature.
@@ -14,61 +31,181 @@ struct Data<'d> {
     key: &'d [u8],
 }
 
-fn main() {
-    const TMP_LEN: usize = 1024 *  16;
-    const KEY_LENGTH: usize = 1024 * 16;
+#[cfg(not(feature = "std"))]
+use cortex_m::peripheral::SYST;
+
+struct Timer<'p> {
+    #[cfg(feature = "std")]
+    start: Instant,
+    #[cfg(not(feature = "std"))]
+    syst: &'p mut SYST,
+}
+
+impl<'p> Timer<'p> {
+    #[cfg(feature = "std")]
+    fn new() -> Self {
+        Self {
+            start: Instant::now(),
+        }
+    }
+
+    #[cfg(not(feature = "std"))]
+    fn new(mut syst: &'p mut SYST) -> Self {
+        syst.enable_interrupt();
+        Self {
+            syst,
+        }
+    }
+
+    fn start(&mut self) {
+        #[cfg(feature = "std")]
+        {
+            self.start = Instant::now();
+        }
+        #[cfg(not(feature = "std"))]
+        {
+            self.syst.set_reload(0x00FFFFFF);
+            self.syst.clear_current();
+            self.syst.enable_counter()
+        }
+    }
+
+    fn end(&mut self) -> Duration {
+        #[cfg(feature = "std")]
+        {
+            self.start.elapsed()
+        }
+        #[cfg(not(feature = "std"))]
+        {
+            Duration::from_micros(SYST::get_current() as u64)
+        }
+    }
+}
+
+const ROUNDS: usize = 300;
+const SAMPLE_COUNT: usize = 10;
+
+#[cfg(feature = "std")]
+type Periphs = ();
+#[cfg(not(feature = "std"))]
+type Periphs = (cortex_m::peripheral::Peripherals, pac::Peripherals);
+
+/// Benchmarks the specified function, returning the median time to run `ROUNDS` of the function
+fn benchmark<'p>(func: BenchFn, data: &mut Data, peripherals: &'p mut Periphs) -> Duration {
+    let mut samples = [Duration::from_secs(0); SAMPLE_COUNT];
+
+    #[cfg(feature = "std")]
+    let mut timer = Timer::new();
+
+    #[cfg(not(feature = "std"))]
+    let mut timer = Timer::new(&mut peripherals.0.SYST);
+    #[cfg(not(feature = "std"))]
+    let b = unsafe { core::ptr::read(&mut peripherals.1.GPIOB).split() };
+    #[cfg(not(feature = "std"))]
+    let mut blue_led = b.pb14.into_push_pull_output();
+    #[cfg(not(feature = "std"))]
+    let mut green_led = b.pb15.into_push_pull_output();
+
+    blue_led.set_low().ok();
+    green_led.set_low().ok();
+    for (sample_num, sample) in samples.iter_mut().enumerate() {
+        let mut total = Duration::from_secs(0);
+        for j in 0..ROUNDS {
+            timer.start();
+            func(data);
+            let millis = timer.end();
+            total += millis;
+        }
+        *sample = total;
+        if sample_num == SAMPLE_COUNT / 16 {
+            blue_led.set_high().ok();
+            green_led.set_high().ok();
+        }
+    }
+    introsort::sort(&mut samples);
+    blue_led.set_high().ok();
+    green_led.set_low().ok();
+
+    //Return median
+    samples[samples.len() / 2]
+}
+
+#[cfg_attr(not(feature = "std"), entry)]
+fn main() -> ! {
+    const TMP_LEN: usize = 1024;
+    const KEY_LENGTH: usize = 1024;
 
     let mut buf = [0u8; TMP_LEN + TMP_LEN + KEY_LENGTH];
     for (i, value) in buf.iter_mut().enumerate() {
         //TODO: Randomly generate
-        *value = core::hint::black_box(i as u8);
+        *value = i as u8;
     }
     let (a, rest) = buf.split_at_mut(TMP_LEN);
-    let (b, key) = rest.split_at_mut(TMP_LEN);
-
-    let bench = Bench::new();
-    let options = Options {
-        iterations: 50,
-        warmup_iterations: 5,
-        min_samples: 25,
-        max_samples: 10000,
-        max_rsd: 5.0,
-        max_duration: None,
-        verbose: false,
-    };
+    let (b, key) = rest.split_at_mut(TMP_LEN); 
 
     let mut data = Data { a, b, key };
     core::hint::black_box(&mut data);
 
-    let functions: [(BenchFn, &'static str); 18] = [
-        (test_aes_128, "AES-128"),
-        (test_aes_192, "AES-192"),
-        (test_aes_256, "AES-256"),
-        (test_blowfish, "Blowfish"),
-        (test_cast5, "Cast5"),
-        (test_des, "DES"),
-        (test_3des, "Triple DES"),
-        (test_idea, "Idea"),
-        (test_kuznyechik, "Kuznyechik"),
-        (test_rc2, "Rc2"),
-        (test_serpent, "Serpent"),
-        (test_sm4, "Sm4"),
-        (test_twofish, "Twofish"),
-        (test_threefish256, "Threefish-256"),
-        (test_threefish512, "Threefish-512"),
-        (test_threefish1024, "Threefish-1024"),
-        (test_xor, "Xor"),
-        (test_xxtea, "XXTEA"),
+    let functions: [(BenchFn, &'static str); 4] = [
+        ////(test_aes_128, "AES-128"),
+        ////(test_aes_192, "AES-192"),
+        ////(test_aes_256, "AES-256"),
+        ////(test_blowfish, "Blowfish"),
+        //(test_cast5, "Cast5"),
+        ////(test_des, "DES"),
+        ////(test_3des, "Triple DES"),
+        //(test_idea, "Idea"),
+        ////(test_kuznyechik, "Kuznyechik"),
+        (test_rc2, "Rc2"),//7.59s, 7.59, 22.92
+        //(test_serpent, "Serpent"),//VERY SLOW
+        (test_sm4, "Sm4"),//7.08, 7.06, 21.16
+        //(test_twofish, "Twofish"),
+        //(test_threefish256, "Threefish-256"),//49.73
+        //(test_threefish512, "Threefish-512"),//46.69
+        //(test_threefish1024, "Threefish-1024"),//47.96
+        (test_xor, "Xor"),//0.??, 0.3, 0.62
+        (test_xxtea, "XXTEA"),//6.35, 6.30, 19.28
     ];
 
+    #[cfg(feature = "std")]
+    let peripherals = ();
+    #[cfg(not(feature = "std"))]
+    let mut peripherals = (cortex_m::peripheral::Peripherals::take().unwrap(), pac::Peripherals::take().unwrap());
     for (func, name) in functions {
-        let res = bench.run(&options, || func(&mut data));
-        let throughput = res.throughput(data.a.len() as _);
+        let median = benchmark(func, &mut data, &mut peripherals);
+        let bytes_per_iteration = TMP_LEN * ROUNDS;
+        let mut bytes_per_sec = bytes_per_iteration as f32 / median.as_secs_f32();
+        //func(&mut data);
+        //let res = bench.run(&options, || );
+        //let throughput = res.throughput(data.a.len() as _);
+        let digits = ["", "K", "M", "G", "T"];
+        let mut digit = 0;
+        while bytes_per_sec > 1000.0 {
+            bytes_per_sec /= 1000.0;
+            digit += 1;
+        }
+        
         //Swap the slices, so that we re-encrypt data for each algorithm
         //This stops the optimizer from realizing that encrypting to the same buffer that gets
         //overridden has no side effects
         core::mem::swap(&mut data.a, &mut data.b);
-        //println!("{} throughput: {}", name, throughput);
+
+        #[cfg(feature = "std")]
+        {
+            println!("{} {:.2} {}B/s", name, bytes_per_sec, digits[digit]);
+        }
+        #[cfg(not(feature = "std"))]
+        {
+            //hprintln!("{} {:.2} {}B/s", name, bytes_per_sec, digits[digit]).unwrap();
+        }
+    }
+    #[cfg(feature = "std")]
+    {
+        std::process::exit(0);
+    }
+    #[cfg(not(feature = "std"))]
+    {
+        loop {}
     }
 }
 
